b8e5911e8a72cf8b9a9590bd39d99b017256dc61
ismoil3 <sufonkulzodaismoil@gmail.com>
2024-12-21 23:45:00 +0500

    edit catalog header

--- src/app/store/catalogHeader/catalogHeader.ts
+++ src/app/store/catalogHeader/catalogHeader.ts
@@ -1,3 +1,4 @@
+import { apiUrl } from "@/config/config";
 import CatalogHeader from "@/types/catalogTypes";
 import axios from "axios";
 import { create } from "zustand";
@@ -6,7 +7,7 @@ export const useCatalogHeader = create<CatalogHeader >((set)=>({
  catalog: [],
  getCatalog: async ()=>{
   try {
-    const {data} = await axios.get(`https://store-api.softclub.tj/Category/get-categories`)
+    const {data} = await axios.get(`${apiUrl}/Category/get-categories`)
         set({catalog: data.data})
   } catch (error) {
     console.error(error);
